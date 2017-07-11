d76f45342421ff71e3603bf5bf4a2e13a4163a6e
Ben Reichert <ben@tozny.com>
2017-07-11 13:52:23 -0700

    initial work

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# setup for npm credentials
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
+
+if [ "$TAG_NEXT" == "true" ]; then
+  # if we want to publish a testing version
+  npm publish --tag next
+else
+  # production publish, uses version in package.json
+  npm publish
+fi
