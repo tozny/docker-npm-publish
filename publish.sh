fa0186a9e28fa8eeb4ddb4e70b2654bbdb12f7e6
Ben Reichert <ben@tozny.com>
2017-07-11 15:55:54 -0700

    install, build, publish

--- publish.sh
+++ publish.sh
@@ -3,12 +3,17 @@
 # setup for npm credentials
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 
+
 if [ "$TAG_NEXT" == "true" ]; then
   # if we want to publish a testing version
-  npm build
-  npm publish --tag next
+  cd /src/ \
+    && npm install --unsafe-perm --no-optional \
+    && npm build \
+    && npm publish --tag next
 else
   # production publish, uses version in package.json
-  npm build
-  npm publish
+  cd /src/ \
+    && npm install --unsafe-perm --no-optional \
+    && npm build \
+    && npm publish
 fi
