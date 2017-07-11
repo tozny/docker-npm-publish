f67b6891419ff2eab2cecfb8ae05b1292ff53d51
Ben Reichert <ben@tozny.com>
2017-07-11 15:36:09 -0700

    Add build steps

--- publish.sh
+++ publish.sh
@@ -5,8 +5,10 @@ echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 
 if [ "$TAG_NEXT" == "true" ]; then
   # if we want to publish a testing version
+  npm build
   npm publish --tag next
 else
   # production publish, uses version in package.json
+  npm build
   npm publish
 fi
